4e4d4cec3ea31622b740f5df89be9a336bdd145a
Vishnu <vishnunarasim20@gmail.com>
2025-02-09 22:53:18 +0530

    Implemented gatewayServer

---
+++ BankGatewayServer/src/main/java/com/bank/BankGatewayServer/BankGatewayServerApplication.java
@@ -0,0 +1,39 @@
+package com.bank.BankGatewayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class BankGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator BankGatewayRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+			.route(p -> p
+				.path("/banks/accounts/**")
+				.filters( f -> f.rewritePath("/banks/accounts/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://ACCOUNTS"))
+			.route(p -> p
+				.path("/banks/loans/**")
+				.filters( f -> f.rewritePath("/banks/loans/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://LOANS"))
+			.route(p -> p
+				.path("/banks/cards/**")
+				.filters( f -> f.rewritePath("/banks/cards/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+				.uri("lb://CARDS"))
+			.build();
+	}
+
+}
